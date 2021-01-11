f217c5e4571bce6c7c55188a67dcf591a537f08b
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2021-01-11 12:47:55 +0100

    updating the onCreateViewHolder(), onBindViewHolder() and bindPhoto

--- app/src/main/java/com/raywenderlich/galacticon/RecyclerAdapter.kt
+++ app/src/main/java/com/raywenderlich/galacticon/RecyclerAdapter.kt
@@ -1,17 +1,52 @@
 package com.raywenderlich.galacticon
 
+import android.content.Intent
+import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
 
 class RecyclerAdapter(private val photos: ArrayList<Photo>) : RecyclerView.Adapter<RecyclerAdapter.PhotoHolder>() {
+
+    class PhotoHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        private var view: View = v
+        private var photo: Photo?= null
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val context = itemView.context
+            val showPhotoIntent = Intent(context, PhotoActivity::class.java)
+            showPhotoIntent.putExtra(PHOTO_KEY, photo)
+            context.startActivity(showPhotoIntent)
+        }
+
+        fun bindPhoto(photo: Photo) {
+            this.photo = photo
+            Picasso.with(view.context).load(photo.url).into(view.itemImage)
+            view.itemDate.text = photo.humanDate
+            view.itemDescription.text = photo.explanation
+        }
+
+        companion object {
+            private val PHOTO_KEY = "PHOTO"
+        }
+
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.PhotoHolder {
-        TODO("Not yet implemented")
+        val inflatedView = parent.inflate(R.layout.recyclerview_item_row, false)
+        return PhotoHolder(inflatedView)
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.PhotoHolder, position: Int) {
-        TODO("Not yet implemented")
+        val itemPhoto = photos[position]
+        holder.bindPhoto(itemPhoto)
     }
 
     override fun getItemCount() = photos.size
